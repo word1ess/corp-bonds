1a7f6600536998f2b82a4f84f207030121c7ce2a
word1ess <antonyashenkov1@gmail.com>
2024-10-17 13:58:23 +0500

    fixes 17.10

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,36 +8,28 @@ import logo from "../../img/logo.svg";
 import Socials from "../Common/Socials/Socials";
 
 function Footer({ isMobile }) {
-  if (isMobile) {
-    return (
-      <footer className="footer">
-        <div className="footer__logo">
-          <Link to="/" className="nav-menu__logo">
-            <img src={logo} alt="logo" />
-          </Link>
-          <nav className="footer__nav">
-            <Link to="#">О проекте</Link>
-            <Link to="#">Политика конфиденциальности</Link>
-          </nav>
-          <p>Copyright © 2024 CorpBonds. Все права защищены.</p>
-        </div>
-        <MenuNav />
-      </footer>
-    );
-  }
   return (
     <footer className="footer">
       <div className="footer__logo">
         <Link to="/" className="nav-menu__logo">
           <img src={logo} alt="logo" />
         </Link>
-        <p>Copyright © 2024 CorpBonds. Все права защищены.</p>
+        {!isMobile && (
+          <p className="footer__copy">
+            Copyright © 2024 CorpBonds. Все права защищены.
+          </p>
+        )}
       </div>
       <MenuNav />
       <nav className="footer__nav">
         <Link to="#">О проекте</Link>
         <Link to="#">Политика конфиденциальности</Link>
       </nav>
+      {isMobile && (
+        <p className="footer__copy">
+          Copyright © 2024 CorpBonds. Все права защищены.
+        </p>
+      )}
       <Socials />
     </footer>
   );
